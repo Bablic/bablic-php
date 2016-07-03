ebb8d7f8a1a114c28fd87e336bc27774ab7662c6
Ishai <ishai@bablic.com>
2016-07-03 10:41:35 +0300

    get meta on startup

--- bablic.php
+++ bablic.php
@@ -79,8 +79,11 @@ class BablicSDK {
             $this->store = new file_store();
         if ($this->store->get('site_id') != '') 
             $this->get_data_from_store();
-		if(!empty($options['site_id']))
+		if(!empty($options['site_id'])){
 			$this->site_id = $options['site_id'];
+			if($this->store->get('site_id') != $this->site_id)
+				$this->get_site_from_bablic();
+		}
         if(!empty($options['subdir']))
             $this->subdir = $options['subdir'];
     }
